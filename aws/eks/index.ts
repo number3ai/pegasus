3136100557d1b838c6a3038d25b03cda529efe4a
Brandon Helms <brandon@golden.co>
2024-08-02 08:23:12 -0500

    nightly

--- aws/eks/index.ts
+++ aws/eks/index.ts
@@ -4,5 +4,3 @@ import "./eks"; // Import and execute the EKS setup
 import "./eks-addons"; // Import and execute the EKS addons configuration
 import "./argocd"; // Import and execute the ArgoCD setup
 import "./outputs"; // Import and execute the outputs configuration
-
-import "./ec2"; // Import and execute the EC2 setup
