17cf1e220a51102e3f8f2e7f7aeb66780dc5adcc
Brandon Helms <brandon@golden.co>
2024-09-07 17:27:16 -0500

    more testing

--- aws/eks/helpers/git-helpers.ts
+++ aws/eks/helpers/git-helpers.ts
@@ -1,29 +1,26 @@
 import * as github from "@pulumi/github";
 import * as pulumi from "@pulumi/pulumi";
 
-import * as crypto from 'crypto';
 import * as yaml from 'js-yaml';
 
 import { Buffer } from 'buffer'; // Node.js built-in module
+import { createHash, randomBytes } from 'crypto';
 
 import { environment, githubRepository } from "../variables";
 
 // Function to convert JSON to YAML and then encode to base64
 function jsonToYamlBase64(jsonObject: object): string {
-  console.log(jsonObject);
-  const yamlString = yaml.dump(jsonObject);
-  console.log(yamlString);
-  const base64String = Buffer.from(yamlString).toString('base64');
-  console.log(base64String);
-  return base64String;
+  return Buffer.from(yaml.dump(jsonObject)).toString('base64');
 }
 
 function generateRandomString(length: number): string {
-  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+  return randomBytes(Math.ceil(length / 2)).toString('hex')
+                                           .slice(0, length);
 }
 
 function hashString(input: string): string {
-  return crypto.createHash('sha1').update(input).digest('hex');
+  return createHash('sha1').update(input)
+                           .digest('hex');
 }
 
 export type GitFileMap = {
@@ -31,9 +28,7 @@ export type GitFileMap = {
   json: object;
 };
 
-// Function to output the results after all the `apply` callbacks have been run
 export function processGitPrFiles(gitPrFiles: Array<GitFileMap>): Array<GitFileMap> {
-  // Since Pulumi ensures that outputs are processed in the right order, you can safely output the result
   pulumi.all(gitPrFiles).apply(() => {
     return gitPrFiles;
   });
@@ -41,39 +36,30 @@ export function processGitPrFiles(gitPrFiles: Array<GitFileMap>): Array<GitFileM
 }
 
 export function createGitPR(branchName: string, files: Array<GitFileMap>) {  
-  console.log(`Creating a new PR for branch: ${branchName}`);
   // Create a new branch from the base branch
   new github.Branch(`${hashString(branchName)}-git-branch`, {  
     repository: githubRepository,
     branch: branchName
   });
-  console.log(`Branch created: ${branchName}`);
+
   for (const file of files) {
-    console.log(`Adding file to branch: ${file.fileName}`);
     const filePath = `/releases/${environment}/${file.fileName}.generated.yaml`;
-    console.log(`File path: ${filePath}`);
-    const content = jsonToYamlBase64(file.json);
-    console.log(`Content: ${content}`);
+
     // Add a new file to the new branch
     new github.RepositoryFile(`${generateRandomString(32)}-git-file`, {
       repository: githubRepository,
       file: filePath,
       branch: branchName,
-      content: content, // Convert content to base64
+      content: jsonToYamlBase64(file.json),
       commitMessage: `Add new file to the repository: ${filePath}`,
     });
-    console.log(`File added: ${file.fileName}`);
-    console.log("--------------------")
   }
 
-  console.log(`Creating a new PR for branch: ${branchName}`);
-  // Create a pull request from the new branch to the base branch
-  const pullRequest = new github.RepositoryPullRequest(`${generateRandomString(32)}-git-pr`, {
+  new github.RepositoryPullRequest(`${generateRandomString(32)}-git-pr`, {
     baseRef: "main",
     baseRepository: githubRepository,
     headRef: branchName,
-    title: `Automated PR from devops pipeline - ${Date.now()}`,
+    title: `Automated PR from devops pipeline - ${Date.now().toString()}`,
     body: "This PR was created automatically by the pegasus bot."
   });
-  console.log(`PR created: ${pullRequest.number}`);
 }
