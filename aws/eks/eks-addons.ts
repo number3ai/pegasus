8f33727fe4ab87bd1b1ff881ec08adad384a3ccc
Brandon Helms <brandon@golden.co>
2024-09-17 10:39:35 -0500

    cleaning up eks addons

--- aws/eks/eks-addons.ts
+++ aws/eks/eks-addons.ts
@@ -1,5 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-
 import * as fs from 'fs';
 
 import { argoCdPrFiles } from "./argocd";
@@ -8,26 +7,28 @@ import { processGitPrFiles, uploadValuesFile } from "./helpers/git";
 const directoryPath = './eks-addons'; 
 const files = fs.readdirSync(directoryPath);
 
-// Output the file names
-const eksAddonsPrFiles = files.map(file => {
-  if (file.endsWith('.ts')) {
+// Process the files asynchronously
+const eksAddonsPrFiles = files
+  .filter(file => file.endsWith('.ts')) // Only process TypeScript files
+  .map(async file => {
     const addon = file.replace('.ts', '');
-  
-    import(`./eks-addons/${addon}`)
-      .then(module => {
-        return processGitPrFiles([module.addon.valueFile]);
-      });
-  }
+    
+    // Dynamically import the module and process the PR files
+    const module = await import(`./eks-addons/${addon}`);
+    return processGitPrFiles([module.addon.valueFile]);
+  });
 
-  return [];
-});
-
-pulumi.all([
-  eksAddonsPrFiles,
-  argoCdPrFiles
-]).apply(([resolvedEksAddonsPrFiles, resolvedArgoCdPrFiles]) => {
-  uploadValuesFile([
-      ...resolvedArgoCdPrFiles,
-      ...resolvedEksAddonsPrFiles.flat() // Flatten the array in case of nested arrays
-  ]);
-});
+// Wait for all eksAddonsPrFiles to be resolved
+Promise.all(eksAddonsPrFiles)
+  .then(resolvedEksAddonsPrFiles => {
+    pulumi.all([argoCdPrFiles, resolvedEksAddonsPrFiles]).apply(([resolvedArgoCdPrFiles, eksAddonsResults]) => {
+      // Flatten the array of results and upload the values file
+      uploadValuesFile([
+        ...resolvedArgoCdPrFiles,
+        ...eksAddonsResults.flat() // Flatten the array in case of nested arrays
+      ]);
+    });
+  })
+  .catch(error => {
+    console.error("Error processing EKS Addon PR files:", error);
+  });
