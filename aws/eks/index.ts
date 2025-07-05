437ee4250d74fec401189eb3f6904112df05e789
Brandon Helms <brandon.helms@dbtlabs.com>
2025-07-05 18:53:19 -0500

    restructure 1

--- aws/eks/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Pegasus EKS Infrastructure
- * Main entry point for the Pegasus EKS infrastructure platform
- */
-
-// Import the main infrastructure from the src directory
-export * from "./src";
